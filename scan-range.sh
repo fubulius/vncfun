a809fcbbea0279a413688d97d172e63621310e9a
fubu <fubu.hades@gmail.com>
2026-05-31 05:54:34 +0200

    Implement periodic VNC scanning in scan-range.sh
    Add loop to continuously scan for VNC on specified range

--- scan-range.sh
+++ scan-range.sh
@@ -2,6 +2,8 @@
 # will get vnc screenshots for your chosen range
 # usage: sudo ./scan-range.sh 200
 # will scan 200.*.*.* for port 5900
+while sleep 20 ; do 
 masscan -p 5900 $1.0.0.0/8 -oG vnc --rate=100000
 awk '/^Host:/ { print $2 }' vnc > vnc-$1
 nmap --script open-curtains.nse -n -Pn -p 5900 -iL vnc-$1
+;done
